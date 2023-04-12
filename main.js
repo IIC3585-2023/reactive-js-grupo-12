89e5cadbc302bae8f1c7803fee6b1b2c2c990496
Benjamín Lillo <benjalso99@gmail.com>
2023-04-12 00:18:26 -0400

    add ghosts

--- main.js
+++ main.js
@@ -23,15 +23,26 @@ const getXPixPos = (x) => (x) * POSWIDTH // de 0 a 17
 const getYPixPos = (y) => (y) * POSHEIGHT // de 0 a 8
 
 // posicion del pacman (indices de la matriz)
-let pacX = getXPixPos(5)
-let pacY = getYPixPos(5)
+let pacX = getXPixPos(0);
+let pacY = getYPixPos(0);
 
+const ghosts = [
+  {
+    x: 5,
+    y: 3
+  },
+  {
+    x: 0,
+    y: 5
+  }
+]
+
+console.log('----', ghosts[0].x)
 
 const drawDot = (y, x) => {
   // crea un punto en la posicion x, y
   const centerX = getXPixPos(x) + POSWIDTH/2
   const centerY = getYPixPos(y) + POSHEIGHT/2
-  console.log(centerX, centerY)
   const radius = 7;
   context.beginPath();
   context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
@@ -39,7 +50,6 @@ const drawDot = (y, x) => {
   context.fill();
   context.lineWidth = 5;
   context.stroke();
-  return context;
 }
 
 const drawDots = () => {
@@ -53,6 +63,24 @@ const drawDots = () => {
   }
 }
 
+const drawGhost = (y, x) => {
+  const centerX = getXPixPos(x) + POSWIDTH/2 + 1
+  const centerY = getYPixPos(y) + POSHEIGHT/2
+  const radius = 15;
+  context.beginPath();
+  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = 'red';
+  context.fill();
+  context.lineWidth = 5;
+  context.stroke();
+}
+
+const drawGhosts = () => {
+  ghosts.forEach((ghost) => {
+    drawGhost(ghost.y, ghost.x);
+  });
+}
+
 const drawPacman = () => {
   // dibuja al pacman en la posicion dada por las variables pacX y pacY
   context.beginPath();
@@ -63,13 +91,15 @@ const drawPacman = () => {
 }
 
 const update = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
   drawDots();
   drawPacman();
+  drawGhosts();
   // en cada frame, modificar las variables pacX, pacY para mover el pacman.
   // para poner/esconder un punto, modificar MAP
 }
-
-setInterval(update, 1000 / 30); // esto actualiza puntos y pacman 60 veces por segundo
+// update();
+setInterval(update, 1000 / 5); // esto actualiza puntos y pacman 5 veces por segundo
 
 
 
